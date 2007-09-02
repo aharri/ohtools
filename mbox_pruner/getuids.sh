e3e5e15f260fc1b16e77ae8c206a928f7ee10eaa
iku <iku>
2007-09-02 13:20:02 +0000

    Import mbox pruner.

---
+++ mbox_pruner/getuids.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# [min,max[
+min=1000
+max=2000
+
+OLD_IFS=$IFS
+
+# newline
+IFS="
+"
+
+if [ -n "$1" ]; then
+	min=$1
+fi
+
+if [ -n "$2" ]; then
+	max=$2
+fi
+
+for line in $(cat /etc/passwd); do
+	uid=$(echo "$line" | cut -f 3 -d ':')
+	if [ "$uid" -ge $min ] && [ "$uid" -lt $max ]; then
+		user=$(echo "$line" | cut -f 1 -d ':')
+		echo "$line"
+		#echo "found uid matching range $min:$max, user is $user"
+	fi
+done
---
+++ mbox_pruner/prune_spams.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+BASE=$(cd -- "$(dirname -- "$0")"; pwd)
+home=/home
+getuids=${BASE}/getuids.sh
+prune=${BASE}/prune.php
+time=31
+
+# check for installed script
+if [ ! -x "$prune" ] || [ ! -x "$getuids" ]; then
+	echo "getuids or prune missing or they have incorrect permissions"
+	exit 1
+fi
+
+# check for readable home directory
+if [ ! -d "$home" ]; then
+	echo "$home is not usable"
+	exit 1
+fi
+
+# main loop
+directories=$($getuids | cut -f 6 -d ':')
+for directory in $directories; do
+	test=$(echo $directory | grep "^${home}/")
+	if [ -z "$test" ]; then
+		continue
+	fi
+
+	spambox="${directory}/mail/spam"
+
+	if [ -f "$spambox" ] && [ ! -f  "${directory}/.no_spam_prune" ]; then
+		echo -n "${directory}: "
+		php -c "${BASE}/php.ini" "$prune" "$time" "$spambox"
+	fi
+done
+
+spambox=~quarantine/mail/inbox
+if [ -f "$spambox" ]; then
+	echo -n "${spambox}: "
+	php -c "${BASE}/php.ini" "$prune" "$time" "$spambox"
+fi
