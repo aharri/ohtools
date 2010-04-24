c8d9b59d1a2e77b6831c626e9c9330245fe2267a
iku <iku>
2010-04-24 13:03:07 +0000

    Fix.

--- maketarball.sh
+++ maketarball.sh
@@ -14,7 +14,7 @@ fi
 
 cd /tmp
 cvs -dopenbsd.fi:/var/cvs -q export -r"$TAG" ohtools
-find ohtools -name .cvsignore -type f -print0 | xargs -0r rm -f
+find ohtools/ -name .cvsignore -type f -print0 | xargs -0r rm -f
 mv ohtools ohtools-"$REL"
 tar zcf "$BASE"/ohtools-"$REL".tar.gz ohtools-"$REL"
 rm -rf ohtools-"$REL"
