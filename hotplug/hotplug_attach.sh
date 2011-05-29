55469b619ffc66db4c3b328cb68f6bf7aa81f801
Antti Harri <iku@openbsd.fi>
2011-05-29 23:24:44 +0300

    - Update hotplug scripts to use DUIDs when matching camera mountpoints. - Bugfixes. - Bump version.

--- hotplug/hotplug_attach.sh
+++ hotplug/hotplug_attach.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #
-# $Id: hotplug_attach.sh,v 1.2 2007/11/09 19:48:20 iku Exp $
-#
-# Copyright (c) 2006,2007 Antti Harri <iku@openbsd.fi>
+# Copyright (c) 2006,2007,2011 Antti Harri <iku@openbsd.fi>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -19,7 +17,7 @@
 #
 # KNOWN BUGS: This is not really a bug of this script,
 # but `install` that comes with OpenBSD. If a directory
-# 2007/09 will be install recursively without either
+# 2007/09 will be installed recursively without either
 # 2007 or 2007/09 existing before install command, then
 # only 09/ will get permissions defined on the command
 # line and 2007/ will get default permissions:
@@ -28,9 +26,8 @@
 # drwxr-xr-x    3 iku      users         512 Sep 30 00:11 2007
 # drwx------    2 iku      users         512 Sep 30 00:11 2007/09
 #
-# CAVEATS: Digicamera part will assume the partition on the
-# flash is 'i' and tries to mount specifically it. Should be
-# pretty easy to adjust to be generic though.
+# Configure your CAMMNT mount point in /etc/fstab, for example:
+# 4f98a293132db4a0.i /mnt/digicam msdos rw,nodev,nosuid,noatime,noexec 0 0
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
@@ -62,14 +59,11 @@ case $DEVCLASS in
 	# that can allocate what ever usb-device is available.
 	# It will create symlinks under /dev that point always
 	# to the right /dev/devname.
-	usbdevs=$(usbdevs -d)
-	linenumber=$(echo "$usbdevs" | sed -n "/$DEVNAME/=")
-	linenumber=$(($linenumber - 1))
-	devicename=$(echo "$usbdevs" | sed -n "${linenumber}p" | tr "[:upper:]" "[:lower:]" | cut -f 2 -d ':')
+	devicename=$(usbdevs -d | fgrep -B 1 "$DEVNAME" | head -n 1 | tr "[:upper:]" "[:lower:]" | cut -f 2 -d ':')
 
 	comparedevs "$devicename" "$PRINTDEVS"
 	if [ "$?" -ne 0 ]; then
-		DEBUG "Could not match \"${devicename}\" against configured printers"
+		DEBUG "Could not match '${devicename}' against configured printers"
 		DEBUG "Printers: $PRINTDEVS"
 		exit 0
 	fi
@@ -83,91 +77,109 @@ case $DEVCLASS in
 	logger "$devicename connected, symlink updated"
 ;;
 2)
-	# Automatic extract for digital cameras.
-	devicename=$(/sbin/disklabel "$DEVNAME" | grep '^label:' | sed -e 's/^label: //' | tr "[:upper:]" "[:lower:]")
+	# This part should be -eu compatible.
+	set -eu
 
-	comparedevs "$devicename" "$CAMDEVS"
-	if [ "$?" -ne 0 ]; then
-		DEBUG "Could not match \"${devicename}\" against configured digicameras"
-		DEBUG "Digital cameras: $CAMDEVS"
-		exit 0
+	duid=$(disklabel "$DEVNAME" | sed -nE 's/^duid: (.*)$/\1/p')
+	if [ -z "$duid" ] || [ "$duid" = "0000000000000000" ]; then
+		DEBUG "No DUID set on '$DEVNAME'"
+		exit 1
+	fi
+	if ! egrep -q "^${duid}\\..[[:space:]]+${CAMMNT}[[:space:]]" /etc/fstab; then
+		DEBUG "DUID ($duid) did not match mount point '${CAMMNT}'"
 	fi
 
 	# Check for JPEG header tool
-	which jhead 1>/dev/null 2>/dev/null
-	if [ "$?" -ne 0 ]; then
+	if ! which jhead 1>/dev/null 2>/dev/null; then
 		logger "JPEG header tool jhead not found."
 		exit 1
 	fi
 
 	# Prepare user & group
-	if [ -n "$CAMUSER" ]; then
-		CAMUSER="-o $CAMUSER"
-	fi
-	if [ -n "$CAMGROUP" ]; then
-		CAMGROUP="-g $CAMGROUP"
-	fi
+	test -n "$CAMUSER" && CAMUSER="-o $CAMUSER"
+	test -n "$CAMGROUP" && CAMGROUP="-g $CAMGROUP"
 
 	# Prepare mount point
 	if [ ! -d "$CAMMNT" ]; then
 		mkdir -p "$CAMMNT"
 	fi
 
-	mount -o 'nodev,nosuid,noatime,noexec' "/dev/${DEVNAME}i" "$CAMMNT"
-	if [ "$?" = 0 ]; then
-		TMP=$(mktemp -t -d hotplug.XXXXXXXXXX) || (logger "Could not create tmpdir"; umount "$CAMMNT"; exit 1)
-		for file in $(find "${CAMMNT}/" \( -iname '*.jpg' -or -iname '*.avi' \)); do
-			install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" "$file" "$TMP" 2>&1 || (logger "Failed to extract $file" ; continue)
-			filename=$(basename "$file")
-			cmp -s "$file" "${TMP}/${filename}"
-			if [ "$?" -eq 0 ]; then
-				rm -f "$file"
-			else
-				logger "Failed to extract file $filename"
-			fi
-		done
-		# Transform file
-		jhead -nf"$CAMFILEFORMAT" "${TMP}/"*.jpg 1>/dev/null
-		year=$(date +%Y)
-		month=$(date +%m)
-
-		# Move jpegs
-		for file in $(find "$TMP" -type f -iname '*.jpg'); do
-			filename=$(basename "$file")
-			exifyear=$(jhead "$file" | fgrep 'Date/Time' | cut -f 2 -d ':' | tr -d ' ')
-			exifmonth=$(jhead "$file" | fgrep 'Date/Time' | cut -f 3 -d ':' | tr -d ' ')
-
-			if [ -z "$exifyear" ]; then
-				exifyear=$year
-			fi
-			if [ -z "$exifmonth" ]; then
-				exifmonth=$month
-			fi
-
-			install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d "${CAMDIR}/${exifyear}/"
-			install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d "${CAMDIR}/${exifyear}/${exifmonth}/"
-			install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" "$file" "${CAMDIR}/${exifyear}/${exifmonth}/"
-			if [ "$?" -eq 0 ]; then
-				rm -f "$file"
-			else
-				logger "Failed to copy file $filename from TMP"
-			fi
-		done
-		# Support for other than jpg follows
-		for file in $(find "$TMP" -type f); do
-			install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d "${CAMDIR}/${year}/"
-			install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d "${CAMDIR}/${year}/${month}/"
-			install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" "$file" "${CAMDIR}/${year}/${month}/"
-			if [ "$?" -eq 0 ]; then
-				rm -f "$file"
-			else
-				logger "Failed to copy file $filename from TMP"
-			fi
-		done
-		rmdir "$TMP" || logger "Files remained in $TMP or unknown error"
-		umount "$CAMMNT"
-	else
-		logger "Failed to mount /dev/${DEVNAME}i under $CAMMNT"
+	if ! mount "$CAMMNT"; then
+		logger "Failed to mount '${CAMMNT}'."
+		exit 1
 	fi
+	trap "umount \"$CAMMNT\"" 0 1 13 15
+
+	TMP=$(mktemp -t -d hotplug.XXXXXXXXXX) || (logger "Could not create tmpdir"; exit 1)
+	files=$(find "${CAMMNT}/" \( -iname '*.jpg' -or -iname '*.avi' \))
+	if [ -z "$files" ]; then
+		logger "No files on device."
+		exit 0
+	fi
+
+	for file in $files; do
+		install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" "$file" "$TMP" 2>&1 || (logger "Failed to extract $file" ; continue)
+		filename=$(basename "$file")
+		if cmp -s "$file" "${TMP}/${filename}"; then
+			rm -f "$file"
+		else
+			logger "Failed to extract file $filename"
+		fi
+	done
+	# Transform file
+	jhead -nf"$CAMFILEFORMAT" "${TMP}/"*.jpg 1>/dev/null
+
+	year=$(date +%Y)
+	month=$(date +%m)
+
+	# Move jpegs
+	for file in $(find "$TMP" -type f -iname '*.jpg'); do
+		filename=$(basename "$file")
+		exifyear=$(jhead "$file" | fgrep 'Date/Time' | cut -f 2 -d ':' | tr -d ' ')
+		exifmonth=$(jhead "$file" | fgrep 'Date/Time' | cut -f 3 -d ':' | tr -d ' ')
+
+		if [ -z "$exifyear" ]; then
+			exifyear=$year
+		fi
+		if [ -z "$exifmonth" ]; then
+			exifmonth=$month
+		fi
+
+		if ! install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d \
+			"${CAMDIR}/${exifyear}/" \
+			"${CAMDIR}/${exifyear}/${exifmonth}/"
+		then
+			logger "Set up '${CAMDIR}/${exifyear}/${exifmonth}/'"
+			continue
+		fi
+		if ! install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" \
+			"$file" \
+			"${CAMDIR}/${exifyear}/${exifmonth}/"
+		then
+			logger "Failed to copy file $filename from TMP"
+			continue
+		fi
+		rm -f "$file"
+	done
+
+	# Support for other than jpg follows
+	for file in $(find "$TMP" -type f); do
+		if ! install $CAMUSER $CAMGROUP -m "$CAMDIRMODE" -d \
+			"${CAMDIR}/${year}/" \
+			"${CAMDIR}/${year}/${month}/"
+		then
+			logger "Set up '${CAMDIR}/${exifyear}/${exifmonth}/'"
+			continue
+		fi
+		if ! install $CAMUSER $CAMGROUP -m "$CAMFILEMODE" \
+			"$file" \
+			"${CAMDIR}/${year}/${month}/"
+		then
+			logger "Failed to copy file $filename from TMP"
+			continue
+		fi
+		rm -f "$file"
+	done
+	rmdir "$TMP" || logger "Files remained in $TMP or unknown error"
 ;;
 esac
